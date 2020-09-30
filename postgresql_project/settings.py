daf047abc584383416be498b5c4821d17d79694b
Muhammad Tariq <s4starb4boy@gmail.com>
2020-09-30 14:49:42 +0500

    'custome_user_authentication'

--- postgresql_project/settings.py
+++ postgresql_project/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Local
     'users.apps.UsersConfig',
     'pages.apps.PagesConfig',
 ]
@@ -131,3 +133,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'home'
