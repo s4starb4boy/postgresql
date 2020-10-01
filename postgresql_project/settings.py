a9d4dd4379f73f8a998da7a39961f61fa6f8523b
Muhammad Tariq <s4starb4boy@gmail.com>
2020-10-01 12:57:33 +0500

    'environment_variable_configured'

--- postgresql_project/settings.py
+++ postgresql_project/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'e^!h7g*62$h+798ba2ngs*%q21-2j0z=y+*tro2ybxv7#18316'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = int(os.environ.get('DEBUG', default=0))
 
 ALLOWED_HOSTS = []
 
